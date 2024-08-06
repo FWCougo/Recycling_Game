39b8d028461eb55bb55bcd8adfc09ec5afb446b6
Frangoroo <161792077+Frangoroo@users.noreply.github.com>
2024-08-06 12:22:04 -0300

    small Stuff

---
+++ My project/Assets/Spawn_Lixo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Lixo : MonoBehaviour
+{
+    public float intervalo;
+
+    IEnumerator SpawnLixo()
+    {
+        yield return null;
+    }
+}
